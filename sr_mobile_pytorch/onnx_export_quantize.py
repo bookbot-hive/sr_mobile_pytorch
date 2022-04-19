819e5b98dd6e02f24b7f008432c64371a56b1aa6
Wilson Wongso <wilsonwong961@gmail.com>
2022-04-19 11:22:08 +0700

    converted generator v4 model

--- sr_mobile_pytorch/onnx_export_quantize.py
+++ sr_mobile_pytorch/onnx_export_quantize.py
@@ -4,7 +4,7 @@ import torch
 
 
 def main():
-    model_checkpoint = "./experiments/generator_v3/model.pth"
+    model_checkpoint = "./experiments/generator_v4/model.pth"
     onnx_model_name = model_checkpoint.replace("pth", "onnx")
     quantized_model_name = model_checkpoint.replace("pth", "quant.onnx")
 
